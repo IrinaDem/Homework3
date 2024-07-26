64d4212aee56da400a1c6e6092c1760c4ce74d8a
IrinaDem <starshinova9297@gmail.com>
2024-07-27 00:26:48 +0300

    Add files via upload

---
+++ lesson4/homework3.py
@@ -0,0 +1,11 @@
+name = "Irina"
+print(name)
+age = 27
+print(age)
+print(age+1)
+new_age = 28
+is_student = "True"
+print("Name: ",name)
+print("Age: ",age)
+print("New Age: ",new_age)
+print("Is Student: ",is_student)
