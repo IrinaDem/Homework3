0502589ae1d32ae26e5fae5beaf69457a7d0aaca
IrinaDem <starshinova9297@gmail.com>
2024-07-28 18:20:15 +0300

    Add files via upload

--- lesson4/homework3.py
+++ lesson4/homework3.py
@@ -2,10 +2,8 @@ name = "Irina"
 print(name)
 age = 27
 print(age)
-print(age+1)
-new_age = 28
+age= 27+1
 is_student = "True"
 print("Name: ",name)
 print("Age: ",age)
-print("New Age: ",new_age)
 print("Is Student: ",is_student)
